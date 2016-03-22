56f3ad7a34d37c1216b3649c7d9fed75d6a6668c
Guin <guinifer.k@gmail.com>
2016-03-22 14:00:29 -0400

    Excercise 2

---
+++ excercise2.rb
@@ -0,0 +1,13 @@
+#problem 1
+good_tip = 0.20  #20% is a good tip
+puts " A good tip for a $55.00 meal would be #{format("$%.2f",55*good_tip)}"
+
+#problem 2
+string_plus_int = "hello world " + 2222.to_s
+puts string_plus_int
+
+#problem 3
+puts "45628 + 7839 = #{45628+7839}"
+
+#problem 4
+puts (10 < 20 && 30 < 20) || !(10 == 11)
